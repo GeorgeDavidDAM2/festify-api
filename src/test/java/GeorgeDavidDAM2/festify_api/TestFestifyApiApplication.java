3657e4d5fea6c87b0b0775d1148f90e56f1d8d4e
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-09-25 13:44:02 +0200

    initial commit

---
+++ src/test/java/GeorgeDavidDAM2/festify_api/TestFestifyApiApplication.java
@@ -0,0 +1,11 @@
+package GeorgeDavidDAM2.festify_api;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestFestifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(FestifyApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
