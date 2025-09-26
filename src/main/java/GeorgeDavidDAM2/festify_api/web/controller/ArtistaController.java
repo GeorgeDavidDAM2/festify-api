8ff6ac75728a42ad90001d1afd36fad27fdbd9fd
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-09-26 10:08:23 +0200

    modificado el pom, agregadas web y dto

---
+++ src/main/java/GeorgeDavidDAM2/festify_api/web/controller/ArtistaController.java
@@ -0,0 +1,9 @@
+package GeorgeDavidDAM2.festify_api.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  // Indica que esta clase es un controlador REST
+public class ArtistaController {
+    public record () {
+    }
+}
