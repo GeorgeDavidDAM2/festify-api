3168650748d43e33cff14d20c806203a035b431f
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-10-03 09:34:18 +0200

    añadida interfaz

--- src/main/java/GeorgeDavidDAM2/festify_api/service/ArtistService.java
+++ src/main/java/GeorgeDavidDAM2/festify_api/service/ArtistService.java
@@ -1,7 +1,13 @@
 package GeorgeDavidDAM2.festify_api.service;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import GeorgeDavidDAM2.festify_api.dto.response.ArtistResponse;
+import java.util.ArrayList;
+
 
 public interface ArtistService { //injection de dependencias
     List<ArtistResponse> listArtists();
+    
 }
--- src/main/java/GeorgeDavidDAM2/festify_api/web/controller/ArtistaController.java
+++ src/main/java/GeorgeDavidDAM2/festify_api/web/controller/ArtistaController.java
@@ -11,29 +11,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import GeorgeDavidDAM2.festify_api.dto.request.CreateArtistRequest;
 import GeorgeDavidDAM2.festify_api.dto.response.ArtistResponse;
+import GeorgeDavidDAM2.festify_api.service.ArtistService;
 
 @RestController  // Indica que esta clase es un controlador REST
-public class ArtistaController {
+public class ArtistaController{ // Implementa la interfaz ArtistService
     /*
     private List<ArtistResponse> artists = List.of(
         new ArtistResponse("ART-001", "Bad bunny", "Trap", "Puerto Rico") // Lista para almacenar los artistas inmutables
     ); 
     */
-    private List<ArtistResponse> artists;
-    public ArtistaController() {  //Creamos un constructor para inicializar la lista de artistas pero es conmutable
-        this.artists = new ArrayList<>();
+     // Inyección de dependencias
+    private final ArtistService artistService;  //creamos una variable de tipo ArtistService que es la interfaz
+
+    //private List<ArtistResponse> artists;
+
+    public ArtistaController(ArtistService artistService) {  // Inyección de dependencias a través del constructor
+        this.artistService = artistService;
+       /*  this.artists = new ArrayList<>();
         this.artists.add(
             new ArtistResponse("ART-1","Bad Bunny", new String[]{"Pop","Trap"},"España",1000000, 25,"Activo")
         );
         this.artists.add(
             new ArtistResponse("ART-2","Box Bunny", new String[]{"Dibujos"},"USA",500000, 10,"Inactivo")
         );
+        */
     }
 
     
     @GetMapping(path="/artists")  // Mapea las solicitudes GET a /artists , es decir, cuando alguien acceda a /artists se ejecutará este método
     public List<ArtistResponse> listArtists() {
-        return artists;
+        return artistService.listArtists(); // Devuelve la lista de artistas
     }
     /* 
    @GetMapping(path="/artists/{id}")  // Mapea las solicitudes GET a /artists/{id} , es decir, cuando alguien acceda a /artists/ART-001 se ejecutará este método
@@ -44,20 +51,20 @@ public class ArtistaController {
             .orElseThrow(() -> new RuntimeException("Artist not found")); // Si no encuentra ningún artista, lanza una excepción
     }
     */
-    /* 
+     
     @GetMapping(path="/artists/{id}")  // Mapea las solicitudes GET a /artists/{id} , es decir, cuando alguien acceda a /artists/ART-001 se ejecutará este método
     public ArtistResponse getArtistById(@PathVariable String id) { // PathVariable indica que el valor de id se obtiene de la URL
-        for (ArtistResponse artist : artists) { // Recorre la lista de artistas
+        for (ArtistResponse artist : artistService.listArtists()) { // Recorre la lista de artistas
             if (artist.id().equals(id)) { // Si el id del artista es igual al id pasado por la URL
                 return artist; // Devuelve el artista
             }
         }
         return null; // Si no encuentra ningún artista, devuelve null 
     }
-    */
+    
     @PostMapping(path="/artists")  // Mapea las solicitudes POST a /artists , es decir, cuando alguien acceda a /artists se ejecutará este método
-    public List<ArtistResponse> createArtist(@RequestBody CreateArtistRequest request) {
-        String id="ART-" + (artists.size()+1); //sumamos 1 al tamaño de la lista para que el id sea unico
+    public List<ArtistResponse> createArtist(@RequestBody CreateArtistRequest request){
+        String id="ART-" + (artistService.listArtists().size()+1); //sumamos 1 al tamaño de la lista para que el id sea unico
         ArtistResponse artist = new ArtistResponse(
             id, //aqui pongo el id que he creado arriba
             request.name(), 
@@ -68,9 +75,9 @@ public class ArtistaController {
             request.estado()
         );
         
-        artists.add(artist);
+        artistService.listArtists().add(artist);
 
-        return artists;
+        return artistService.listArtists(); // Devuelve la lista de artistas actualizada
         //he encontrado un metodo request, que seriviria para crear un artista con los datos que le pasas
         //luego en el postman debemos ir al body e introducir los parametros en tipo json
     }
