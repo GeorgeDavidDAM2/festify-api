e15fe1bbd414b56f726cf554962ca9526161c1ed
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-10-06 12:33:28 +0200

    agregado el applicattion de base de datos

---
+++ src/main/java/GeorgeDavidDAM2/festify_api/persistence/jpa/entity/ArtistEntity.java
@@ -0,0 +1,78 @@
+package GeorgeDavidDAM2.festify_api.persistence.jpa.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "artists")
+public class ArtistEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private List<String> genres;
+    private String country;
+    private Double listeners;
+    private String status;
+
+    public ArtistEntity() {
+    }
+
+    public ArtistEntity(Long id, String name, List<String> genres, String country, Double listeners, String status) {
+        this.id = id;
+        this.name = name;
+        this.genres = genres;
+        this.country = country;
+        this.listeners = listeners;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Double getListeners() {
+        return listeners;
+    }
+
+    public void setListeners(Double listeners) {
+        this.listeners = listeners;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
